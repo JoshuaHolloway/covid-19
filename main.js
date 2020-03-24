ab8e3c8a9784fd55d405d94691208957b2b6df31
JoshuaHolloway <jhollow6@asu.edu>
2020-03-24 12:58:03 -0500

    growth factor

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const data_arr = [];
 const y_axis_labels = [];
 const x_axis_data__confirmed = [];
+const x_axis_data__confirmed__change = [];
+const x_axis_data__confirmed__growth_factor = [];
 const url = 'https://pomber.github.io/covid19/timeseries.json';
 fetch(url)
     .then(res => res.json())
@@ -10,11 +12,31 @@ fetch(url)
             y_axis_labels.push(elem.date);
             x_axis_data__confirmed.push(elem.confirmed);
         });
+
+        for (let i = 1; i < x_axis_data__confirmed.length; ++i) {
+            const x0 = x_axis_data__confirmed[i-1];
+            const x1 = x_axis_data__confirmed[i];
+            const dx = x1 - x0;
+            x_axis_data__confirmed__change.push(dx);
+        }
+
+        for (let i = 1; i < x_axis_data__confirmed__change.length; ++i) {
+            const dx0 = x_axis_data__confirmed__change[i-1];
+            const dx1 = x_axis_data__confirmed__change[i];
+            const growth_factor = dx1 / dx0;
+            if (growth_factor < 1e3)
+                x_axis_data__confirmed__growth_factor.push(growth_factor);
+            else
+                x_axis_data__confirmed__growth_factor.push(null);
+        }
+
+        console.log(x_axis_data__confirmed__change);
+        console.log(x_axis_data__confirmed__growth_factor);
     })
     .then(() => {
         chart_callback__log();
         chart_callback__linear();
-        
+        chart_callback__growth();
     });
 //===============================================
 const chart_callback__linear = () => {
@@ -80,18 +102,17 @@ const chart_callback__linear = () => {
     };
 
 
-    // If clicked on then update window display
-    const pill_1 = document.getElementById('pill-1');
-    pill_1.addEventListener('click', () => {
-        const ctx = document.getElementById('canvas').getContext('2d');
-        window.myLine = new Chart(ctx, config);
-    });
-
     // Update display on window load
+     const ctx = document.getElementById('canvas-linear').getContext('2d');
     window.onload = function() {
-        const ctx = document.getElementById('canvas').getContext('2d');
         window.myLine = new Chart(ctx, config);
     };
+
+    // If clicked on then update window display
+    const pill = document.getElementById('pill-1');
+    pill.addEventListener('click', () => {
+        window.myLine = new Chart(ctx, config);
+    });
 
     document.getElementById('randomizeData').addEventListener('click', function() {
         config.data.datasets.forEach(function(dataset) {
@@ -164,20 +185,6 @@ const chart_callback__log = () => {
                 borderColor: window.chartColors.red,
                 data: x_axis_data__confirmed,
                 fill: false,
-            }, {
-                label: 'My Second dataset',
-                fill: false,
-                backgroundColor: window.chartColors.blue,
-                borderColor: window.chartColors.blue,
-                data: [
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor()
-                ],
             }]
         },
         options: {
@@ -214,9 +221,71 @@ const chart_callback__log = () => {
         }
     };
 
-    const pill_2 = document.getElementById('pill-2');
-    pill_2.addEventListener('click', () => {
+    const pill = document.getElementById('pill-2');
+    pill.addEventListener('click', () => {
         const ctx = document.getElementById('canvas-log').getContext('2d');
+        window.myLine = new Chart(ctx, config);
+    });
+
+
+};
+//===============================================
+const chart_callback__growth = () => {
+    let config = {
+        type: 'line',
+        data: {
+            labels: y_axis_labels,
+            datasets: [{
+                label: 'Change',
+                backgroundColor: window.chartColors.red,
+                borderColor: window.chartColors.red,
+                data: x_axis_data__confirmed__change,
+                fill: false,
+            }, {
+                label: 'Growth Factor',
+                fill: false,
+                backgroundColor: window.chartColors.blue,
+                borderColor: window.chartColors.blue,
+                data: x_axis_data__confirmed__growth_factor,
+            }]
+        },
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Month'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    type: 'logarithmic',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value',
+                    },
+                }]
+            }
+        }
+    };
+
+    const pill = document.getElementById('pill-3');
+    pill.addEventListener('click', () => {
+        const ctx = document.getElementById('canvas-growth').getContext('2d');
         window.myLine = new Chart(ctx, config);
     });
 
