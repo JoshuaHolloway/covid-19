c2c70adc7d12a26ecc6b3c3867d5c029817f98d6
JoshuaHolloway <jhollow6@asu.edu>
2020-03-25 14:24:31 -0500

    mirroring works witht the desired input

--- shift.js
+++ shift.js
@@ -1,49 +1,27 @@
 // ==============================================
-const size = y => {
-    const N = y.length;
-    return [(N-1) / 2, N];
-};
-// ==============================================
-const shift = (y, offset, N) => {
+const shift = (y, offset) => {
     let y_shifted = [];
-    for (let i = 0; i < N; ++i)
+    for (let i = 0; i < y.length; ++i)
         y_shifted.push(y[i] - offset);
     return y_shifted;
 };
 // ==============================================
-const truncate = (y, N_size) => {
-    let y_trunc = [];
-    for (let i = 0; i <= N_size; ++i)
-        y_trunc.push(y[i]);
-    return y_trunc;
-};
-// ==============================================
-const un_offset = (y_mirrored, offset, N) => {
-
-    for (let i = 0; i < N; i++)
+const un_offset = (y_mirrored, offset) => {
+    for (let i = 0; i < y_mirrored.length; i++)
         y_mirrored[i] += offset;
     return y_mirrored;
 };
 // ==============================================
 const mirror = y => {
-
-    const [N_half, N] = size(y);
-    const offset = y[N_half];
-
-    const y_shifted = shift(y, offset, N);
-    const y_trunc = truncate(y_shifted, N_half);
-
-    const y_mirrored = y_trunc.slice();
-
-    for (let i = N_half-1; i >= 0; i--) 
-        y_mirrored.push(-y_trunc[i]);
-
-    const y_final = un_offset(y_mirrored, offset, N);
-
-    return y_final;
+    const N = y.length;
+    const offset = y[N-1];
+    const y_shifted = shift(y, offset);
+    for (let i = N/2; i >= 0; i--) 
+        y_shifted.push(-y_shifted[i]);
+    return un_offset(y_shifted, offset, N);
 };
 // ==============================================
-let y = [0,1,2,4,6,7,8];
-
+let y_gold = [0,1,2,4,6,7,8];
+let y = [0,1,2,4];
 y_mirrored = mirror(y);
 console.log(y_mirrored);
