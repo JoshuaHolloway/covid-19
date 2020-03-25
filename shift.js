f8179fd1f060cfe14d592a11dd619bfcc0eb5cca
JoshuaHolloway <jhollow6@asu.edu>
2020-03-25 13:51:53 -0500

    shifting and mirroring toy problem

--- shift.js
+++ shift.js
@@ -1,5 +1,5 @@
 // ==============================================
-const half = (x, y) => {
+const size = (x, y) => {
     if (x.length !== y.length)
         return null;
 
@@ -13,8 +13,8 @@ const half = (x, y) => {
 // ==============================================
 const shift = (x, y) => {
 
-    const [N_half, N] = half(x, y);
-    const center = [x[N_half], y[N_half]];
+    const [N_size, N] = size(x, y);
+    const center = [x[N_size], y[N_size]];
     console.log(center);
 
     let x_shifted = [];
@@ -24,28 +24,36 @@ const shift = (x, y) => {
         y_shifted.push(y[i] - center[1]);
     }
 
-    console.log(x_shifted);
-    console.log(y_shifted);
-
     return [x_shifted, y_shifted];
 };
 // ==============================================
 const truncate = (x, y) => {
 
-    const [N_half, N] = half(x, y);
+    const [N_size, N] = size(x, y);
     let x_trunc = [];
     let y_trunc = [];
-    for (let i = 0; i <= N_half; ++i) {
+    for (let i = 0; i <= N_size; ++i) {
         x_trunc.push(x[i]);
         y_trunc.push(y[i]);
     }
     return [x_trunc, y_trunc];
 };
 // ==============================================
-x = [0,1,2,3,4,5,6];
-y = [0,1,2,4,6,7,8];
-[x_shifted, y_shifted] = shift(x, y);
-[x_trunc, y_trunc] = truncate(x_shifted, y_shifted);
+const mirror = (x_shifted, y_shifted) => {
 
-console.log(x_trunc);
-console.log(y_trunc);
+    [x_shifted, y_shifted] = shift(x, y);
+    [x_trunc, y_trunc] = truncate(x_shifted, y_shifted);
+
+    let y_mirrored = y_trunc.slice();
+    y_mirrored.push(-y_trunc[2]);
+    y_mirrored.push(-y_trunc[1]);
+    y_mirrored.push(-y_trunc[0]);
+
+    return y_mirrored;
+};
+// ==============================================
+let x = [0,1,2,3,4,5,6];
+let y = [0,1,2,4,6,7,8];
+
+y_mirrored = mirror(x, y);
+console.log(y_mirrored);
