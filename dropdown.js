2a0fb0d6debb63e507edb6f9ee02e44ddd48db76
JoshuaHolloway <jhollow6@asu.edu>
2020-04-01 13:26:13 -0500

    select region dropdowns

---
+++ dropdown.js
@@ -0,0 +1,17 @@
+console.log('josh');
+
+$('.dropdown-menu a').on('click', function(){
+    // $('#datebox').val($(this).text());
+    console.log('clicked');
+
+    // const x = document.getElementById('dropdownMenuButton').innerHTML = 'JOSH';
+
+
+    // // $('#datebox').val($(this).html());
+    // console.log($('dropdown-menu').val($(this).html()));
+});
+
+$('#dropdown-menu li').on('click', function(){
+    // $('#datebox').val($(this).text());
+    console.log('clicked');
+});
